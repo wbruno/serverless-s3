1014c0ae66fee52dfc24b4a0986cd4d8dd6ce70b
William Bruno <wbrunom@gmail.com>
2025-08-08 09:42:05 -0300

    chore: reduce wait time

--- docker/run_test.sh
+++ docker/run_test.sh
@@ -2,4 +2,4 @@ TARGET=$1
 npx yarn --cwd /src install
 npx yarn --cwd /src workspace ${TARGET} install
 npx yarn --cwd /src workspace ${TARGET} run start &
-/src/docker/wait-for-it.sh  -t 600 localhost:3000 -- npx yarn --cwd /src workspace ${TARGET} run test
+/src/docker/wait-for-it.sh  -t 60 localhost:3000 -- npx yarn --cwd /src workspace ${TARGET} run test
