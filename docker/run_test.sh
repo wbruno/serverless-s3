cbb98856d39a44c25622aafd3006baa834cab4c7
William Bruno <wbrunom@gmail.com>
2025-08-08 09:45:42 -0300

    chore: remove unecessary workspace

--- docker/run_test.sh
+++
@@ -1,5 +0,0 @@
-TARGET=$1
-npx yarn --cwd /src install
-npx yarn --cwd /src workspace ${TARGET} install
-npx yarn --cwd /src workspace ${TARGET} run start &
-/src/docker/wait-for-it.sh  -t 60 localhost:3000 -- npx yarn --cwd /src workspace ${TARGET} run test
